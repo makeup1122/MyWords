5b6e4020d8502d7bedda1bad616b292c8bef351e
makeup1122 <makeup1123@163.com>
2017-01-09 20:10:12 +0800

    添加jsonp提交至服务器

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const http = require('http');
 const fs   = require('fs');
 const url  = require('url');
+const mysql = require('./mysql');
+//加载配置文件
+const config = require('./config');
 
 var server = http.createServer(function (req, res) {
     let params = url.parse(req.url,true);
+    console.log('request pathname:'+params.pathname);
     if(params.pathname == '/'){
         let html  = fs.readFileSync('mywords.html');
         res.write(html);
         res.end();
-    }else{
+    }else if(params.pathname =='/save'){
         //jsonP
-        res.end(params.query.callback+"("+JSON.stringify({'dsadsadasdas':'libing'})+")");
+        if(params.query.errorCode == 0){
+            mysql.save(params.query.query,params.query['translation[]']);
+            msg = 'success! save=['+params.query.query+']' ;
+        }else{
+            msg = 'nothingToDo!';
+        }
+        res.end(params.query.callback+"("+JSON.stringify({msg:msg})+")");
+    }else{
+        res.writeHead(404);
+        res.end();
     }
 });
 
---
+++ mysql.js
@@ -0,0 +1,48 @@
+const mysql = require('mysql');
+const config = require('./config');
+var connection =  mysql.createConnection(config.database);
+connection.connect();
+exports.select = function(sql=''){
+    if(sql == null ||sql ==''){return false;}
+    connection.connect();
+    connection.query(sql, function(err, rows, fields) {
+    if (err) throw err;
+        console.log('The solution is: ', rows[0].word);
+    });
+    connection.end();
+}
+exports.save = function(query='',translation='',callback){
+    if(query == null ||query ==''){return false;}
+    // connection.connect();
+    has_query_history(query,function(id){
+        // console.log(id);
+        if(id){
+            let sql = 'update mywords set times = times+1 ,last_time="" where id = "' +id+'"';
+            // console.log(sql);
+            connection.query(sql, function(err, rows, fields) {
+            if (err) throw err;
+                // console.log('The solution is: ', rows[0].word);
+            });
+        }else{
+            let sql = "INSERT INTO `test`.`mywords`(`word`,`translation`,`last_time`)VALUES('"+query.trim()+"','"+translation.trim()+"','');";
+            // console.log(sql);
+            connection.query(sql, function(err, rows, fields) {
+            if (err) throw err;
+                // console.log('The solution is: ', rows[0].word);
+            });
+        }
+    });
+}
+function has_query_history(query,callback){
+    let sql = 'select id from mywords where word = "'+ query.trim()+'"';
+    // console.log(sql);
+    connection.query(sql, function(err, rows, fields) {
+        if (err) throw err;
+        if(rows == '' || rows[0].id <= 0){
+            callback(null);
+        }else{
+            callback(rows[0].id);
+        }
+    });
+    // connection.end();
+}
