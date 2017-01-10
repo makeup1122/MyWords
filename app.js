5d47039326279223c20b865bb001148a9598fad0
makeup1122 <makeup1123@163.com>
2017-01-10 10:03:25 +0800

    fix

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const http = require('http');
 const fs   = require('fs');
 const url  = require('url');
 const mysql = require('./mysql');
+const webhookHandler = require('github-webhook-handler');
+var webhook = webhookHandler({path:'/webhook',secret:config.webhook.secret})
 //加载配置文件
 const config = require('./config');
-
+//创建Server
 var server = http.createServer(function (req, res) {
     var par = url.parse(req.url,true);
     console.log('request pathname:'+par.pathname);
@@ -26,8 +28,15 @@ var server = http.createServer(function (req, res) {
         res.end();
     }
 });
-
-server.listen(23544,'0.0.0.0',function(){
+//webhook事件
+webhook.on('error',function(err){
+    console.log("error:",err.message);
+})
+webhook.on('push',function(event){
+    console.log(event);
+})
+//监听端口
+server.listen(config.app.port,'0.0.0.0',function(){
     console.log('Server running at http://0.0.0.0:'+server.address().port+'/');
 });
 
--- config.js
+++ config.js
@@ -1,11 +1,16 @@
 var config = {
-    app:{},
+    app:{
+        port:8888
+    },
     database:{
         host:'127.0.0.1',
         port:'3306',
         user:'root',
         password:'',
         database:'test'
+    },
+    webhook:{
+        secret:'test'
     }
 };
 module.exports = config;
